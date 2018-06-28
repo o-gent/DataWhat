4dcb07a8be9a050089a85249313ae4ecb6d8fc80
O Gent <38910643+o-gent@users.noreply.github.com>
2018-06-28 11:51:22 +0100

    First "working" release
    Send despacito love for days!

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 
 # Sends despacito view count to friends on whatsapp!! 
-Download chromeweb driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put in same folder as main.py
+
+Could actually be useful if you sent useful information, right?
+
+planned features
+- [] facebook messenger support
+- [] optimise so chrome only runs when sending message
+- [] custom youtube video and message 
+
+Fixes needed
+- [] save cookies so dont need to scan QR code each time
+
+# SETUP
+Download chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put in same folder as main.py
 Install selenium using `pip install selenium`
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import pickle
 
 import time
 import os
@@ -11,38 +12,48 @@ from scraper import despacito
 def main():
     # ask user and frequency 
     # ask if run before
-    run = input('have you run this before? answer y/anything else')
-    user_name = input('put exact username of person you want to Whatsacito')
-    if run == "y":
-        not_first_run(user_name)
-    else:
-        first_run(user_name)
+    run = input('have you run this before? answer y/anything else ---> ')
+    user_name = input('put exact username of person you want to Whatsacito ---> ')
+    frequency = int(input('how often do you want to send despacito messages? (in seconds) --> '))
+    if frequency < 10:
+        print('some people just want to watch the world burn.. you must really hate the other person')
     
-    pass
+    if run == "y":
+        not_first_run()
+    else:
+        driver = first_run()
+    n = 0
+    while True:
+        find_user(driver, user_name)
+        n += 1
+        print('{0} cycles made'.format(n))
+        time.sleep(frequency)
 
-def first_run(user_name):
+def first_run():
     # will need to pair whatsapp web with phone whatsapp, then login should be saved in cookies
     driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
     driver.get("https://web.whatsapp.com/")
     answer = input('Is the phone connected successfully? (y/Y) -> ')
-    if str(answer).strip().lower() == 'y':
-        keep_running = True
-        while keep_running:
-            try:
-                find_user(driver, user_name)
-            except:
-                keep_running = False
+    pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
 
-def not_first_run(user_name):
+    if str(answer).strip().lower() == 'y':
+        return driver
+
+def not_first_run():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
+    driver.get("http://www.google.com")
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    time.sleep(5)
     driver.get("https://web.whatsapp.com/")
-    find_user(driver, user_name)
+    return driver
 
 def find_user(driver, user_name):
     user_name = user_name.strip()
-    message = "despacito has " + str(despacito()) + "and counting!"
+    message = "despacito has " + str(despacito()) + " and counting!"
 
     user_name_list = user_name.split(',')
 
@@ -56,7 +67,8 @@ def open_chat(driver, user_name):
         web_obj = driver.find_element_by_xpath("//input[@title='Search or start new chat']")
         web_obj.send_keys(user_name)
         time.sleep(2)
-        element = driver.find_element_by_xpath('//span[contains(text(),"{0}")]'.format(user_name))
+        element = driver.find_element_by_xpath('//*[@title="{0}"]'.format(user_name))
+        # ('//span[contains(text(),"{0}")]'.format(user_name))
         element.click()
         return True
     except:
