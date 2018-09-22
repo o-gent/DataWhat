a655b4b51cb5f61b20ca1827bb3855fa96541584
O Gent <38910643+o-gent@users.noreply.github.com>
2018-09-22 18:28:19 +0100

    organisation
    implimented command>arg1,arg2 style

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ cli.add_func(add)
 usr = ['put_usr_here']
 datawhat = datawhat(usr, cli)
 datawhat.send_message('hello, type /help for list of functions, start each command with /')
-datawhat.send_message('arguments are seperated by spaces, for example:     /add 60 9')
+datawhat.send_message("arguments follow the command by '>' and are seperated by commas, for example:     /add>60,9")
 
 while True:
     datawhat.command_input()
--- pycli/cli_main.py
+++ pycli/cli_main.py
@@ -1,30 +1,18 @@
 """ core file for the pycli module """
 from inspect import signature
 
-def usr_parse(string):
-    """ internal function: gets function and parameters from string """
-    split = string.split()
-    func = split[0]
-    try: args = split[1:]
-    except: pass
-
-    for i in range(len(args)):
-        try: args[i] = int(args[i])
-        except: pass
-    
-    return func, args
-
 
 class cli(object):
     def __init__(self):
-        self.function_list = {'stop': [self.stop, 0], 'help': [self.help, 1]}
+        self.function_list = {'stop': [self.stop, 0], 'help': [self.cli_help, 1]}
         self.is_active = False
+        print('functions have style --  func>arg1,arg2')
 
     def start(self):
         print('type help for a list of available commands!')
         self.is_active = True
         while self.is_active:
-            func, args = usr_parse(input('--> '))
+            func, args = self.usr_parse(input('--> '))
             # note use of * so func parses list as individual params
             try: 
                 self.function_list[func][0](*args)
@@ -33,14 +21,14 @@ class cli(object):
                    print('function failed, function has args: ', list(signature(func).parameters.keys()))
                 except:            
                    print('not a function!')
-    
-    def input(self, usr_input):
+
+    def cli_input(self, usr_input):
         """ takes string as input and executes command """
-        func, args = usr_parse(usr_input)
+        func, args = self.usr_parse(usr_input)
         # note use of * so func parses list as individual params
         try: return self.function_list[func][0](*args)
         except: return "failed"
-
+    
     def stop(self):
         self.is_active = False
         print('CLI stopping')
@@ -49,7 +37,7 @@ class cli(object):
         try: self.function_list[function.__name__] = function, list(signature(function).parameters.keys())
         except: print('not a function!')
     
-    def help(self, *args):
+    def cli_help(self, *args):
         if len(args) == 0:
             print(self.function_list.keys())
             return str(self.function_list.keys())
@@ -59,3 +47,19 @@ class cli(object):
                 print(self.function_list[i][1])
                 l += self.function_list[i][1]
             return l
+
+    def usr_parse(self, string):
+        """ internal function: gets function and parameters from string """
+        split = string.split('>')
+        func = split[0]
+
+        try: args = split[1]
+        except: pass
+        try: args = args.split(',')
+        except: pass
+
+        for i in range(len(args)):
+            try: args[i] = int(args[i])
+            except: pass
+        
+        return func, args
